e7832c5fae8be40f2cde00446d3bed9bf33c2fec
siaa20 <sia20@aber.ac.uk>
2018-08-18 01:17:42 +0200

    testing temp

--- src/sia20/senosrs/mpu9250/Tester.java
+++ src/sia20/senosrs/mpu9250/Tester.java
@@ -1,5 +1,7 @@
 package sia20.senosrs.mpu9250;
 
+import java.io.IOException;
+
 public class Tester {
 
     private MPU9250 mpu;
@@ -16,20 +18,34 @@ public class Tester {
     }
 
     private void test(){
-        this.standardSetup();
-        String[] dataLabels = {"AccelXH", "AccelXL", "AccelYH", "AccelYL", "AccelZH", "AccelZL", "TempH", "TempL", "GyroXH", "GyroXL", "GyroYH", "GyroYL", "GyroZH", "GyroZL"};
+        //String[] dataLabels = {"AccelXH", "AccelXL", "AccelYH", "AccelYL", "AccelZH", "AccelZL", "TempH", "TempL", "GyroXH", "GyroXL", "GyroYH", "GyroYL", "GyroZH", "GyroZL"};
+        /*for (int i = 0; i < data.length; i++) {
+            System.out.println(dataLabels[i] + ": " + data[i]);
+        }*/
         String[] dataLabesHalf = {"AccelX", "AccelY", "AccelZ", "Temp", "GyroX", "GyroY", "GyroZ"};
         byte[] data = mpu.readSensors();
-        for (int i = 0; i < data.length; i++) {
-            System.out.println(dataLabels[i] + ": " + data[i]);
-        }
         for (int i = 0; i < dataLabesHalf.length; i++) {
-            System.out.println(dataLabesHalf[i] + ": " + ((data[i]<<8 |0xF) & data[i+1]));
+            System.out.println(dataLabesHalf[i] + ": " + ((data[i] << 8 | 0xF) & data[i + 1]));
         }
     }
+
+    private void readTemp(){
+        int rawTemp = ((mpu.read(0x41)<<8 | 0xF) & mpu.read(0x42));
+        double temp = (rawTemp/333.87) + 21;
+        System.out.println("Temp: " + temp);
+    }
+
     public static void main(String[] args){
         Tester test = new Tester();
         System.out.println("Hello space!");
-        test.test();
+        //test.test();
+        test.standardSetup();
+        try {
+            while (System.in.available() == 0){
+                test.readTemp();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
