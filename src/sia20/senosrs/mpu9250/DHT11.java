273abe627f4215131d9801fe6a7305eef7c847fd
siaa20 <sia20@aber.ac.uk>
2018-08-29 16:39:05 +0200

    added dht11 sensor

---
+++ src/sia20/senosrs/mpu9250/DHT11.java
@@ -0,0 +1,92 @@
+package sia20.senosrs.mpu9250;
+
+import com.pi4j.wiringpi.Gpio;
+import com.pi4j.wiringpi.GpioUtil;
+
+class DHT11 {
+    private static final int    MAXTIMINGS  = 85;
+    private final int[]         dht11_dat   = { 0, 0, 0, 0, 0 };
+
+    DHT11() {
+
+        // setup wiringPi
+        if (Gpio.wiringPiSetup() == -1) {
+            System.out.println(" ==>> GPIO SETUP FAILED");
+            return;
+        }
+
+        GpioUtil.export(3, GpioUtil.DIRECTION_OUT);
+    }
+
+    void getData(final int pin) {
+        int lastState = Gpio.HIGH;
+        int j = 0;
+        dht11_dat[0] = dht11_dat[1] = dht11_dat[2] = dht11_dat[3] = dht11_dat[4] = 0;
+
+        Gpio.pinMode(pin, Gpio.OUTPUT);
+        Gpio.digitalWrite(pin, Gpio.LOW);
+        Gpio.delay(18);
+
+        Gpio.digitalWrite(pin, Gpio.HIGH);
+        Gpio.pinMode(pin, Gpio.INPUT);
+
+        for (int i = 0; i < MAXTIMINGS; i++) {
+            int counter = 0;
+            while (Gpio.digitalRead(pin) == lastState) {
+                counter++;
+                Gpio.delayMicroseconds(1);
+                if (counter == 255) {
+                    break;
+                }
+            }
+
+            lastState = Gpio.digitalRead(pin);
+
+            if (counter == 255) {
+                break;
+            }
+
+            /* ignore first 3 transitions */
+            if (i >= 4 && i % 2 == 0) {
+                /* shove each bit into the storage bytes */
+                dht11_dat[j / 8] <<= 1;
+                if (counter > 16) {
+                    dht11_dat[j / 8] |= 1;
+                }
+                j++;
+            }
+        }
+        // check we read 40 bits (8bit x 5 ) + verify checksum in the last
+        // byte
+        if (j >= 40 && checkParity()) {
+            float h = (float) ((dht11_dat[0] << 8) + dht11_dat[1]) / 10;
+            if (h > 100) {
+                h = dht11_dat[0]; // for DHT11
+            }
+            float c = (float) (((dht11_dat[2] & 0x7F) << 8) + dht11_dat[3]) / 10;
+            if (c > 125) {
+                c = dht11_dat[2]; // for DHT11
+            }
+            if ((dht11_dat[2] & 0x80) != 0) {
+                c = -c;
+            }
+            System.out.println("Humidity: " + h + " Temperature: " + c);
+        } else {
+            System.out.println("Data not good, skip");
+        }
+
+    }
+
+    private boolean checkParity() {
+        return dht11_dat[4] == (dht11_dat[0] + dht11_dat[1] + dht11_dat[2] + dht11_dat[3] & 0xFF);
+    }
+
+    public static void main(final String ars[]) throws Exception {
+        final DHT11 dht = new DHT11();
+        for (int i = 0; i < 10; i++) {
+            Thread.sleep(2000);
+            dht.getData(7);
+        }
+        System.out.println("Done!!");
+    }
+}
--- src/sia20/senosrs/mpu9250/Tester.java
+++ src/sia20/senosrs/mpu9250/Tester.java
@@ -1,6 +1,7 @@
 package sia20.senosrs.mpu9250;
 
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
 
 public class Tester {
 
@@ -39,26 +40,69 @@ public class Tester {
             e.printStackTrace();
         }
     }
-    public static void main(String[] args){
-        Tester test = new Tester();
-        System.out.println("Hello space!");
-        //test.test();
-        test.standardSetup();
+
+    private Double[][] read(){
+        ArrayList<ArrayList<Double>> data = new ArrayList<>();
+        long timeZero = System.nanoTime();
         try {
             while (System.in.available() == 0){
+                ArrayList<Double> tempData = new ArrayList<>();
                 System.out.print("\033[H\033[2J");
                 System.out.flush();
-                double temp = test.temp.readTemp();
-                double[] gyro = test.gyroscope.readGyro();
+                double newTemp = temp.readTemp();
+                double[] gyro = gyroscope.readGyro();
+                tempData.add((double)(System.nanoTime()-timeZero));
+                for (double tempGyro : gyro) {
+                    tempData.add(tempGyro);
+                }
                 System.out.println("Temp: " + temp);
                 System.out.println("Gyro");
                 System.out.println("\tx: " + gyro[0]);
                 System.out.println("\ty: " + gyro[1]);
                 System.out.println("\tz: " + gyro[2]);
-                test.sleep(20);
+                sleep(20);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+        Double[][] doneData = data.toArray(new Double[data.size()][]);
+        return doneData;
+    }
+    private void save(Double[][] data){
+        FileWriter fw = null;
+        try {
+            fw = new FileWriter("./data.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        for (Double[] oneRead : data) {
+            for (Double oneData: oneRead) {
+                try {
+                    fw.write(oneData.toString() + ", ");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    public static void main(String[] args){
+
+        final DHT11 dht = new DHT11();
+        for (int i = 0; i < 10; i++) {
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            dht.getData(7);
+        }
+            System.out.println("Done!!");
+        /*
+        Tester test = new Tester();
+        System.out.println("Hello space!");
+        //test.test();
+        test.standardSetup();
+        Double[][] data = test.read();
+        */
     }
 }
